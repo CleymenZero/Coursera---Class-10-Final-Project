b063344cfdc575719b354024436f5a74ba3055b4
CleymenZero <54853545+CleymenZero@users.noreply.github.com>
2024-08-28 17:08:51 -0400

    spacex_dash_app.py

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -23,10 +23,8 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 dcc.Dropdown(
                                     id='site-dropdown',
                                     options=[
-                                                     {'label': 'All Sites', 'value': 'ALL'},
-                                                     {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
-                                                     {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'},
-                                                     {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
+                                                     {'label': 'All Sites', 'value': 'ALL'}, {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+                                                     {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'}, {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
                                                      {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'}
                                                      ],
                                     value='ALL',
